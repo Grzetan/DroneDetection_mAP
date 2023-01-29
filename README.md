6875da210c0a4ebc0613978cfbb2a313b9a05627
Grzetan <grzetan@gmail.com>
2023-01-29 16:57:38 +0100

    Tested on 3rd dataset

---
+++ README.md
@@ -0,0 +1,7 @@
+# Metric Calculator
+
+MetricCalculator is a tool for calculating common computer vision metrics, such as Mean Average Precision (mAP), Number of False Negatives (FN), and Mean Distance between Centers of Bounding Boxes (MDBC).
+
+```bash
+python3 main.py
+```
--- main.py
+++ main.py
@@ -17,6 +17,13 @@ def load_predicions(path: str):
             boxes.append([int(row[0])] + box)
     return np.array(boxes)
 
+# frame_id, x1, y1, x2, y2
+def load_labels(path: str):
+    data = list(csv.reader(open(path)))
+    data = [[int(val) for val in row] for row in data[1:]]
+
+    return np.array(data)
+
 def load_predictions2(path: str):
     data = list(csv.reader(open(path)))
     boxes = []
@@ -40,21 +47,29 @@ def load_labels2(path: str):
             boxes.append(box)
     return np.array(boxes)
 
-# frame_id, x1, y1, x2, y2
-def load_labels(path: str):
-    data = list(csv.reader(open(path)))
-    data = [[int(val) for val in row] for row in data[1:]]
+def load_labels3(path: str, shift: int):
+    data = list(csv.reader(open(path), delimiter=','))
+    boxes = []
+    for row in data[1:]:
+        for i in range((len(row)-1) // 4):
+            box = [int(float(a)) for a in row[i*4+1:i*4+5]]
+            box = [int(row[0]) - shift] + box
+            boxes.append(box)
+    return np.array(boxes)
 
-    return np.array(data)
+
 
 def main():
     # preds = load_predicions("./predictions2/Dron T02.61920488.20220117151609.csv")
     # labels = load_labels("./labels2/Dron T02.61920488.20220117151609.avi.csv")
 
-    preds = load_predictions2("./predictions/coordinates_1.csv")
-    labels = load_labels2("./labels/connected_1.csv")
+    # preds = load_predictions2("./predictions/coordinates_1.csv")
+    # labels = load_labels2("./labels/connected_1.csv")
 
-    score = metrics(preds, labels, mAP_start=0.3, mAP_stop=0.4, mAP_step=0.1, main_iou_thresh=0.2, plot=False)
+    labels = load_labels3("./labels3/Dron T02.55260362.20220117151609.avi.csv", 3)
+    preds = load_predicions("./predictions3/best_640T0.2/Dron T02.55260362.20220117151609.csv")
+
+    score = metrics(preds, labels, mAP_start=0.5, mAP_stop=0.95, mAP_step=0.05, main_iou_thresh=0.5, plot=True)
     print(score)
 
     # Save to CSV file
@@ -62,5 +77,3 @@ def main():
     df.to_csv("output.csv")
 
 if __name__ == "__main__": main()
-
-# output do csv ???
