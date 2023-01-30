c6969963c2add1d4606a81bb2a5c463ac3c5e40a
grzetan <grzetan@gmail.com>
2023-01-30 08:59:42 +0100

    Visualized dataset

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import csv
 from collections import Counter
-from metrics import metrics
+from metrics import metrics, iou
+import cv2
+import random
 
 # Box = frame_id, x1, y1, x2, y2, prob_score
 def load_predicions(path: str):
@@ -66,14 +68,34 @@ def main():
     # preds = load_predictions2("./predictions/coordinates_1.csv")
     # labels = load_labels2("./labels/connected_1.csv")
 
-    labels = load_labels3("./labels3/Dron T02.55260362.20220117151609.avi.csv", 3)
+    labels = load_labels3("./labels3/Dron T02.55260362.20220117151609.avi.csv", 4)
     preds = load_predicions("./predictions3/best_640T0.2/Dron T02.55260362.20220117151609.csv")
 
-    score = metrics(preds, labels, mAP_start=0.5, mAP_stop=0.95, mAP_step=0.05, main_iou_thresh=0.5, plot=True)
-    print(score)
+    height = 800
+    width = 1000
+    scale = 0.5
 
-    # Save to CSV file
-    df = pd.DataFrame([score])
-    df.to_csv("output.csv")
+    random.shuffle(labels)
+
+    for i, label in enumerate(labels[-5:]):
+        image = np.zeros((height,width,3), np.uint8)
+        # Label
+        image = cv2.rectangle(image, (int(label[1] * scale), int(label[2] * scale)), (int(label[3] * scale), int(label[4] * scale)), (0,255,0), 1)
+
+        # Load predictions
+        ps = [p for p in preds if p[0] == label[0]]
+        for p in ps:
+            image = cv2.rectangle(image, (int(p[1] * scale), int(p[2] * scale)), (int(p[3] * scale), int(p[4] * scale)), (0,0, 255), 1)
+            cv2.putText(image, f"{round(iou(np.array(p[1:-1]), np.array(label[1:])), 2)}", (int(p[3]*scale), int(p[2]*scale)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow(f"{label[0]}", image)
+        cv2.waitKey(0)
+
+    # score = metrics(preds, labels, mAP_start=0.5, mAP_stop=0.95, mAP_step=0.05, main_iou_thresh=0.5, plot=True)
+    # print(score)
+
+    # # Save to CSV file
+    # df = pd.DataFrame([score])
+    # df.to_csv("output.csv")
 
 if __name__ == "__main__": main()
