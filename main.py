7547591090c7b28ae307306a5c384a5df04726f3
Grzetan <grzetan@gmail.com>
2023-02-02 10:30:36 +0100

    Calcualted metrics for AirSim data

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def visualizeDataset(preds: np.ndarray, labels: np.ndarray, scale: float = 0.5):
 
     random.shuffle(labels)
 
-    for i, label in enumerate(labels[:5]):
+    for i, label in enumerate(labels[-5:]):
         image = np.zeros((height,width,3), np.uint8)
         # Label
         image = cv2.rectangle(image, (int(label[1] * scale), int(label[2] * scale)), (int(label[3] * scale), int(label[4] * scale)), (0,255,0), 1)
@@ -104,45 +104,33 @@ def main():
 
     # New data
 
-    label_dir = "./labels3"
-    predictions_dir = "./predictions3"
+    label_dir = "./labels4_lab_15"
+    predictions_dir = "./AirSim/lab_15"
 
-    labels = load_labels('./labels4_lab_12/labelsCam8.csv')
-    preds = load_predicions('./AirSim/lab_12/airSim_640T0.2/cam8i_s.csv')
+    models = [p for p in os.listdir(predictions_dir) if p.startswith('airSim') and not p.endswith('onnx')]
 
-    result = metrics(preds, labels)
-    print(result)
+    all_metrics = []
 
-    # visualizeDataset(preds, labels)
+    for model in models:
+        print(model)
+        cams = sorted([p for p in os.listdir(os.path.join(predictions_dir, model)) if p.endswith('.csv')])
+        mAPs = []
+        FN_counts = []
+        mean_distances = []
+        for i, cam in enumerate(cams):
+            print(cam)
+            labels = load_labels(os.path.join(label_dir, f'labelsCam{i+1}.csv'))
+            preds = load_predicions(os.path.join(predictions_dir, model, cam))
 
-    # labels_files = [l for l in os.listdir(label_dir) if l.endswith('.avi.csv')]
-    
-    # all_metrics = []
+            result = metrics(preds, labels)
+            mAPs.append(result['mAP'])
+            FN_counts.append(result['FN_count'])
+            mean_distances.append(result['mean_center_dist'])
+        
+        all_metrics.append([model, sum(mAPs) / len(mAPs), sum(FN_counts), sum(mean_distances) / len(mean_distances)])
 
-    # for path in os.listdir(predictions_dir):
-    #     files = os.listdir(os.path.join(predictions_dir, path))
-    #     mAPs = []
-    #     FN_counts = []
-    #     mean_distances = []
-
-    #     for l in labels_files:
-    #         pred_file = [f for f in files if f.split('.csv')[0] == l.split('.avi.csv')[0]][0]
-    #         print(os.path.join(label_dir, l), os.path.join(predictions_dir, path, pred_file))
-
-    #         preds = load_predicions(os.path.join(predictions_dir, path, pred_file))
-    #         labels = load_labels3(os.path.join(label_dir, l), 4)
-
-    #         result = metrics(preds, labels, mAP_start=0.5, mAP_stop=0.9, mAP_step=0.05, main_iou_thresh=0.5, plot=False)
-
-    #         mAPs.append(result['mAP'])
-    #         FN_counts.append(result['FN_count'])
-    #         mean_distances.append(result['mean_center_dist'])
-
-    #     all_metrics.append([path, sum(mAPs) / len(mAPs), sum(FN_counts), sum(mean_distances) / len(mean_distances)])
-
-    # df = pd.DataFrame(all_metrics, columns=['Model', 'mAP', 'FN_count', 'mean_center_distance'])
-    # df.to_csv('output.csv')
-
+    df = pd.DataFrame(all_metrics, columns=['Model', 'mAP', 'FN_count', 'mean_center_distance'])
+    df.to_csv('metrics_lab15.csv')
 
     # Old data
 
@@ -181,7 +169,7 @@ def main():
 
     # Visualize dataset
 
-    # labels = load_labels3("./labels3/Dron T02.55260362.20220117151609.avi.csv", 4)
+    # labels = load_labels3("./labels3/Dron T02.55260362.20220117151609.avi.csv", 3)
     # preds = load_predicions("./predictions3/best_640T0.2/Dron T02.55260362.20220117151609.csv")
 
     # visualizeDataset(preds, labels)
